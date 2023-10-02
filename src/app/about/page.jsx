5af779cb44df69e10bf677a6fe1502358f94f142
sonny <santino.soltani99@gmail.com>
2023-10-02 13:29:24 +0330

    adding about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,81 @@
+import SectionWrapper from '../components/SectionWrapper';
+const AboutPage = () => {
+  return (
+    <SectionWrapper styles={'flex flex-col gap-6 mt-6'}>
+      <h1 className="text-4xl font-bold text-primary-color">About</h1>
+      <article>
+        <p className="text-dark-bright-text-color text-justify text-lg leading-7">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec
+          iaculis massa. Curabitur luctus accumsan sapien, id porta leo placerat
+          sit amet. Pellentesque vestibulum scelerisque libero facilisis
+          fermentum. Duis sed velit dignissim, lacinia diam eu, tristique leo.
+          Sed nulla ante, efficitur quis convallis sed, iaculis suscipit sem.
+          Donec rhoncus, neque in ornare bibendum, urna nunc lacinia risus, in
+          eleifend nisl nulla non nulla. Nunc sem nibh, sagittis ut mi sed,
+          ultricies porttitor turpis. Duis nibh odio, ultrices nec elit et,
+          euismod euismod lorem. Nullam interdum faucibus orci, eu vulputate
+          urna pellentesque non. Proin quis condimentum purus. Sed imperdiet,
+          velit a vehicula elementum, lacus risus egestas ipsum, sed imperdiet
+          nisl eros a erat. Cras quis ligula libero. Etiam eget ligula at justo
+          varius aliquet a eu sapien. Aliquam vestibulum dictum sem, sit amet
+          dictum augue elementum sit amet. Duis porta leo pharetra nisi mollis
+          malesuada. Donec bibendum purus sodales, dapibus mauris eu, ornare
+          ligula. Cras posuere dolor non tortor interdum, id imperdiet turpis
+          placerat. Fusce metus tortor, euismod quis ligula a, tincidunt rutrum
+          ex. Nam sed porttitor arcu. Aliquam massa massa, consectetur in enim
+          et, maximus sollicitudin urna. Vestibulum lectus turpis, condimentum
+          nec condimentum non, ultricies eget nisi. Nulla sollicitudin a lorem
+          eu hendrerit. Curabitur vitae maximus neque. Donec non mollis lacus.
+          Sed at nisl accumsan, suscipit tellus ac, elementum urna. Nunc id
+          pharetra ipsum, vitae posuere quam. In commodo, erat nec aliquet
+          ultricies, tortor nulla gravida tellus, eu ultrices risus massa
+          ultricies sapien. Vestibulum non maximus nisi. Vivamus dapibus
+          faucibus nulla eu mattis. Integer vel ultricies eros. Integer varius,
+          orci nec ornare ornare, elit ipsum fermentum lacus, sit amet imperdiet
+          velit nunc sed massa. Sed elit arcu, molestie at facilisis sit amet,
+          imperdiet id turpis. Sed sollicitudin nisl porttitor ultricies
+          blandit. Praesent egestas lobortis dui, at facilisis diam. Nullam eros
+          mauris, lacinia quis nunc at, gravida blandit arcu. Nullam ac sagittis
+          purus, non vulputate ipsum. Sed convallis, est in hendrerit dapibus,
+          nisl tellus aliquam magna, at varius odio lorem vitae tortor.
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+          posuere cubilia curae; Etiam interdum tortor eget efficitur imperdiet.
+          Aliquam erat volutpat. Sed tincidunt fermentum lectus. Suspendisse
+          maximus quam sed imperdiet scelerisque. Vestibulum ante ipsum primis
+          in faucibus orci luctus et ultrices posuere cubilia curae; Nullam
+          mollis augue ut rutrum sodales. In condimentum justo at sapien feugiat
+          congue. Morbi elementum nisl non viverra congue. Nullam vehicula
+          porttitor quam, non molestie urna molestie ac. Mauris dictum nisl a
+          neque dapibus faucibus id eu eros. Pellentesque consectetur ante et
+          ligula consequat, et lobortis nunc lacinia. Praesent auctor ut metus
+          eget faucibus. Phasellus magna odio, semper in iaculis a, interdum sit
+          amet elit. Nulla in diam in magna tempus rutrum ut ut odio. Duis vitae
+          dolor eu ex iaculis cursus. Vestibulum ante ipsum primis in faucibus
+          orci luctus et ultrices posuere cubilia curae; Maecenas in tempor
+          orci. Praesent dolor felis, faucibus eu porttitor non, sagittis id
+          diam. Vivamus non est vitae diam consequat ornare eu ut dui. Aenean
+          ornare mauris quis risus vestibulum tempor. Praesent in aliquam dolor,
+          nec mollis tortor. Pellentesque habitant morbi tristique senectus et
+          netus et malesuada fames ac turpis egestas. Nullam hendrerit, mi vitae
+          dapibus porttitor, eros lorem auctor massa, non tristique purus elit
+          sit amet ipsum. Suspendisse posuere diam at nisl porttitor, ut pretium
+          mi sagittis. Pellentesque eu magna vitae ex volutpat porttitor. Fusce
+          neque urna, bibendum sit amet efficitur ac, varius in lorem. Aliquam
+          vulputate quis ante vitae porttitor. Morbi vel feugiat enim. Etiam
+          consectetur tellus commodo ipsum euismod, vitae vulputate elit
+          interdum. Sed vitae mollis eros. Donec ex leo, euismod eu mauris ac,
+          pretium accumsan urna. Fusce diam ipsum, lacinia eget orci at,
+          ullamcorper feugiat ante. Integer eu lorem vel nibh vehicula
+          ullamcorper sed in enim. Integer mi purus, placerat nec nisl vel,
+          convallis sagittis urna. Cras venenatis ac ex quis imperdiet. Vivamus
+          felis eros, faucibus vel pellentesque in, semper vel elit. Suspendisse
+          vel consequat ligula. Aenean lacinia ante sed leo fringilla, eu
+          molestie diam efficitur. Phasellus non enim a lacus viverra tempus
+          consectetur sit amet tortor.
+        </p>
+      </article>
+    </SectionWrapper>
+  );
+};
+
+export default AboutPage;
