43e089073ded9bc7dfb5e152966c71c8e9e7b25a
sonny <santino.soltani99@gmail.com>
2023-10-04 14:18:35 +0330

    add simple error handling in fetch

--- src/app/lib/products-util.js
+++ src/app/lib/products-util.js
@@ -11,13 +11,19 @@ const fetchOptions = {
   },
 };
 
+// Get All Function
 export const getAllProducts = async (sort, page) => {
-
   const res = await fetch(
     `${productsRoute}/${ sort || 'popular' }?api_key=${API_KEY}&language=en-US&page=${page || '1'}`,
     { next: { revalidate: 60 * 60 * 24 } },
     fetchOptions
-  );
+  )
+  
+  // Error Handling
+  if(!res.ok){
+    throw new Error('failed to fetch data')
+  }
+  // Storing Data
   const data = await res.json();
 
   return data;
