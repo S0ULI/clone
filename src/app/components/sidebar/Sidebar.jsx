ec119b874d04a777def9dc353d10a691de1c516e
sonny <santino.soltani99@gmail.com>
2023-10-07 05:59:38 +0330

    fixig sidebar

--- src/app/components/sidebar/Sidebar.jsx
+++ src/app/components/sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ const SideBar = () => {
     <div className="flex bg-background-color-c dark:bg-black/90 w-full p-4 rounded-xl">
       <SidebarItem title="Popular" params="popular" />
       <SidebarItem title="Top Rated" params="top_rated" />
-      <SidebarItem title="Up Coming" params="upcoming" />
+      <SidebarItem title="Coming" params="upcoming" />
     </div>
   );
 };
