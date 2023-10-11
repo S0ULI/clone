1a6ad18d417078ac51f00062ec8a41ea447f96bb
abolfazl <souli.fsd@gmail.com>
2023-10-11 04:35:50 +0330

    cart badge in header

--- src/app/components/header/NavItem.jsx
+++ src/app/components/header/NavItem.jsx
@@ -1,14 +1,28 @@
 import Link from 'next/link';
 
-const NavItem = ({ title, link, Icon }) => {
+const NavItem = ({ title, link, Icon, badge }) => {
   // const activeClass = pathname && pathname === link && 'text-secondary-color';
   return (
     <Link
       href={link}
       className="w-full py-4 border-b border-white/10 sm:border-none text-nav-list-color dark:text-dark-nav-list-color hover:text-nav-list-color-hover dark:hover:text-dark-nav-list-color-hover transition-all duration-300 "
     >
-      <div className="flex gap-4 items-center"><span>{title}</span><span className='block sm:hidden text-3xl'><Icon/></span></div>
-      
+      <div className="flex gap-4 items-center w-full">
+        <div
+          className={`flex items-center gap-4 sm:gap-1 w-full ${badge ? 'justify-between' : ''}`}>
+          <div className='flex gap-4'>
+            <span>{title}</span>
+            <span className="block sm:hidden text-3xl">
+              <Icon />
+            </span>
+          </div>
+          {badge ? (
+            <span className="sm:text-xs text-md bg-dark-background-color-p text-nav-list-color-hover rounded-full block w-10 h-10 leading-10 sm:w-6 sm:h-6 sm:leading-6 text-center self-end">
+              {badge}
+            </span>
+          ) : null}
+        </div>
+      </div>
     </Link>
   );
 };
--- src/app/components/header/Navbar.jsx
+++ src/app/components/header/Navbar.jsx
@@ -1,8 +1,15 @@
+'use client'
+
+import { useContext } from 'react';
+import CartContext from '@/app/store/cart-context';
 import {BsArrowRight} from 'react-icons/bs'
 import NavItem from './NavItem';
 import DarkModeSwitch from './DarkModeSwitch';
 
 const Navbar = () => {
+  const cartCtx = useContext(CartContext);
+  const cartItems = cartCtx.totalProducts;
+
   return (
     <>
     <nav className='w-full'>
@@ -10,7 +17,7 @@ const Navbar = () => {
         <NavItem title="About" link="/about" Icon={BsArrowRight}/>
         <NavItem title="Products" link="/products" Icon={BsArrowRight}/>
         <NavItem title="Home" link="/" Icon={BsArrowRight}/>
-        <NavItem title="Cart" link="/cart" Icon={BsArrowRight}/>
+        <NavItem title="Cart" link="/cart" Icon={BsArrowRight} badge={cartItems}/>
       </ul>
     </nav>
     <div className='flex justify-start sm:hidden w-full'>
