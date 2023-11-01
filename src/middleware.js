4aabbb258675d327fe754e33c343c43d2ad49d55
abolfazl <souli.fsd@gmail.com>
2023-11-01 20:36:11 +0330

    fix

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,24 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
+import { authMiddleware } from '@clerk/nextjs';
+
 export default authMiddleware({
-    publicRoutes: ['/', '/about', ,'/products', `/products/:productId`, '/api/products', '/api/products/:productId', '/api', '/api/webhooks/user', '/api/search', '/api/search/:searchTerm', '/search', '/search/:searchTerm', '/cart']
+  publicRoutes: [
+    '/',
+    '/about',
+    ,
+    '/products',
+    `/products/:productId`,
+    '/api/products',
+    '/api/products/:productId',
+    '/api',
+    '/api/webhooks/user',
+    '/api/search',
+    '/api/search/:searchTerm',
+    '/search',
+    '/search/:searchTerm',
+    '/cart',
+  ],
 });
- 
+
 export const config = {
-      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
- 
